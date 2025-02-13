0a3c6745542420da60d23cf90f65540adadd3b0c
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2025-02-13 09:20:56 +0000

    環境変数の追加と反映

--- dns/main.py
+++ dns/main.py
@@ -1,18 +1,22 @@
+from os import environ as env
+
 # from dnslib import DNSRecord, QTYPE
 import socket
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-  server.bind(("0.0.0.0", 5353))
-  server.listen()
-  print("DNS server (tcp) is Running")
-  conn, _ = server.accept()
-  with conn:
-    while True:
-      data = conn.recv(1024)
-      if not data:
-        break
-      print(f"Received: {data}")
-      # conn.sendall(data)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+  client.bind((env[""], 53535));
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.bind(("0.0.0.0", 5353))
+    server.listen()
+    print("DNS server (tcp) is Running")
+    conn, _ = server.accept()
+    with conn:
+      while True:
+        data = conn.recv(1024)
+        if not data:
+          break
+        print(f"Received: {data}")
+        # conn.sendall(data)
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server:
   server.bind(("0.0.0.0", 5353))
--- rails/run.sh
+++ rails/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-rm -f tmp/pids/server.pid &&  rails s -b 0.0.0.0
+rm -f tmp/pids/server.pid && rails s -b 0.0.0.0
 sleep infinity
