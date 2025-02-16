82ecabb9fa4eed1b0ba71050ca2bd6420eb759c8
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2025-02-16 16:25:18 +0000

    DHCPのバイナリデータ変換用クラスの微調整（ブロードキャストに対応？）

--- dhcp/main.py
+++ dhcp/main.py
@@ -3,6 +3,8 @@ import socket
 from my_modules import MyDHCP, erase_data, search_from_client_data, search_from_ip, set_data
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server:
+  server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
   server.bind(("0.0.0.0", 67))
   print(f"DHCP server is Running on {socket.gethostbyname(socket.gethostname())}:{67}")
   while True:
