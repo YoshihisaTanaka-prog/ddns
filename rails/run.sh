4c6e80f72193f386bc1c71b397bd7864665543b1
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2025-02-16 14:11:32 +0000

    他環境導入時の不整合を修正

--- rails/run.sh
+++ rails/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 cd $(dirname $0)
-rm -f tmp/pids/server.pid && bundle exec rails s -b 0.0.0.0
+rm -f tmp/pids/server.pid && bundle config --global path vendor/bundle && bundle exec rails s -b 0.0.0.0
 sleep infinity
